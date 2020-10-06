a2559f1986430063a050f5e219543e6885b7bbef
Scott Ricketts <SJR@imac.lan>
2020-10-06 14:28:13 -0500

    Add code; nothing works

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,30 +1,57 @@
+// Store API json for Earthquakes
+var queryUrl =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Create tile layer for background
-var lightmap = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY,
-  }
-);
-
-// Create a baseMaps object to hold the lightmap layer
-var baseMaps = {
-    "Light Map": lightmap
-};
-
-// Create the map object with options
-var map = L.map("mapid", {
-  center: [40.73, -74.0059],
-  zoom: 12,
-  layers: [lightmap],
+// Use D3 to request Earthquake Info
+d3.json(queryUrl, function(data) {
+  createFeatures(data.features);
+  console.log(data);
 });
 
-// Create a layer control and add to the map
-L.control.layers(baseMaps, {
-    collapsed: false,
-  })
-  .addTo(map);
+function createFeatures(earthquakeData) {
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +"</h3><hr><p>" + new Date(feature.properties.time) +"</p>"
+    );
+  }
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+  // Create tile layer for background
+  var lightmap = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY,
+    }
+  );
+
+  // Create a baseMaps object to hold the lightmap layer
+  var baseMaps = {
+    "Light Map": lightmap,
+  };
+
+  // Create the map object with options
+  var map = L.map("mapid", {
+    center: [40.73, -74.0059],
+    zoom: 12,
+    layers: [lightmap],
+  });
+
+  // Create a layer control and add to the map
+  L.control
+    .layers(baseMaps, {
+      collapsed: false,
+    })
+    .addTo(map);
+}
