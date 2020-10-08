1819ec3528da055d899ae8e2fbe8614d388db3a1
Scott Ricketts <SJR@imac.lan>
2020-10-08 15:50:35 -0500

    Change color error to depth from mag

--- Leaflet-Step-1/static/js/logic2.js
+++ Leaflet-Step-1/static/js/logic2.js
@@ -16,11 +16,6 @@ var baseMaps = {
   "Light Map": lightmap,
 };
 
-// Create overlay object to hold our overlay layer
-// var overlayMaps = {
-//   Earthquakes: earthquakes,
-// };
-
 // Create the map object with options
 var map = L.map("mapid", {
   center: [39.8283, -98.5795],
@@ -28,78 +23,78 @@ var map = L.map("mapid", {
   layers: [lightmap],
 });
 
-// Create a layer control and add to the map
-// L.control
-//   .layers(baseMaps, overlayMaps, {
-//     collapsed: false,
-//   })
-//   .addTo(map);
-
 lightmap.addTo(map);
 
 d3.json(
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
-      function styleInfo(feature) {
-        return {
-          radius: getRadius(feature.properties.mag),
-          fillColor: getColor(feature.properties.mag),
-          color: "#000000",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8,
-        };
+    // style for circles
+    function styleInfo(feature) {
+      return {
+        radius: getRadius(feature.properties.mag),
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "#000000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
       };
+    }
 
-      function getRadius(magnitude) {
-        return magnitude * 4;
-      };
+    // create radius for circles
+    function getRadius(magnitude) {
+      return magnitude * 4;
+    }
+    // create different colors for depth
+    function getColor(depth) {
+      switch (true) {
+        case depth > 5:
+          return "#810f7c";
+        case depth > 4:
+          return "#810f7c ";
+        case depth > 3:
+          return "#8856a7";
+        case depth > 2:
+          return "#8c96c6";
+        case depth > 1:
+          return "#b3cde3";
+        default:
+          return "#edf8fb";
+      }
+    }
 
-      function getColor(magnitude) {
-        switch (true) {
-            case magnitude > 5:
-                return "red";
-            case magnitude > 4:
-                return "yellow";
-            case magnitude > 3:
-                return "green";
-            case magnitude > 2:
-                return "blue";
-            case magnitude > 1:
-                return "brown";
-            default:
-                return "white";
-        }
-      };
-
-      L.geoJson(data, {
-          pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng)
-          },
-          style: styleInfo, 
-          onEachLayer: function(feature, layer) {
-              layer.bindPopup("Magnitude: " + feature.properties.mag + "<br> Location: " + feature.properties.place);
-          }
-      }).addTo(map);
+    L.geoJson(data, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+      style: styleInfo,
+      onEachLayer: function (feature, layer) {
+        layer.bindPopup(
+          "Magnitude: " +
+            feature.properties.mag +
+            "<br> Depth: " +
+            feature.geometry.coordinates[2]
+        );
+      },
+    }).addTo(map);
 
     var legend = L.control({ position: "bottomright" });
 
     legend.onAdd = function (map) {
-    var div = L.DomUtil.create("div", "info legend"),
-        mags = [0, 1, 2, 3, 4, 5]
+      var div = L.DomUtil.create("div", "info legend"),
+        mags = [0, 1, 2, 3, 4, 5];
 
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < mags.length; i++) {
+      // loop through intervals and generate a colored square for each
+      for (var i = 0; i < mags.length; i++) {
         div.innerHTML +=
-        '<i style="background:' +
-        getColor(mags[i] + 1) +
-        '"></i> ' +
-        mags[i] +
-        (mags[i + 1] ? "&ndash;" + mags[i + 1] + "<br>" : "+");
-    }
+          '<i style="background:' +
+          getColor(mags[i] + 1) +
+          '"></i> ' +
+          mags[i] +
+          (mags[i + 1] ? "&ndash;" + mags[i + 1] + "<br>" : "+");
+      }
 
-    return div;
+      return div;
     };
 
     legend.addTo(map);
-});
+  });
 
