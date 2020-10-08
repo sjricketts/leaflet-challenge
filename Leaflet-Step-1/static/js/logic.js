aeacda250a8da04be02d10af6335299f0c7c1a98
Scott Ricketts <SJR@imac.lan>
2020-10-08 15:52:49 -0500

    Rename file

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,74 +1,100 @@
-// Create the map
-
-function createMap(earthquakes) {
-  // Create tile layer for background
-  var lightmap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/light-v10",
-      accessToken: API_KEY,
-    }
-  );
-
-  // Create a baseMaps object to hold the lightmap layer
-  var baseMaps = {
-    "Light Map": lightmap,
-  };
-
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes,
-  };
-
-  // Create the map object with options
-  var map = L.map("mapid", {
-    center: [39.8283, -98.5795],
-    zoom: 4,
-    layers: [lightmap],
-  });
-
-  // Create a layer control and add to the map
-  L.control
-    .layers(baseMaps, overlayMaps, {
-      collapsed: false,
-    }).addTo(map);
-}
-
-
-// Create Markers for Map
-function createMarkers(response) {
-  // Get info
-  var earthquakes = response.features;
-
-  // Initialize array
-  var quakeMarkers = [];
-  
-  // Loop through earthquakes array
-  for (var index = 0; index < earthquakes.length; index++) {
-    var earthquake = earthquakes[index];
-
-    // Create circle marker and bind popup for each earthquake
-    var quakeMarker = L.circleMarker([
-      earthquake.geometry.coordinates[1],
-      earthquake.geometry.coordinates[0],
-      ]).bindPopUp("<h3>Magnitude: " + earthquake.properties.mag +
-        "</h3><br><h3>Depth: " + earthquake.geometry.coordinates[2] + "</h3>"
-    );
-
-    // Add marker to the array
-    quakeMarkers.push(quakeMarker);
+var lightmap = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY,
   }
-  // Create layer group and add to createMap function
-  createMap(L.layerGroup(quakeMarkers));
-}
+);
+
+// Create a baseMaps object to hold the lightmap layer
+var baseMaps = {
+  "Light Map": lightmap,
+};
+
+// Create the map object with options
+var map = L.map("mapid", {
+  center: [39.8283, -98.5795],
+  zoom: 4,
+  layers: [lightmap],
+});
+
+lightmap.addTo(map);
 
 d3.json(
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
-    createMap(data.feature);
-  }
-);
+    // style for circles
+    function styleInfo(feature) {
+      return {
+        radius: getRadius(feature.properties.mag),
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "#000000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+      };
+    }
+
+    // create radius for circles
+    function getRadius(magnitude) {
+      return magnitude * 4;
+    }
+    // create different colors for depth
+    function getColor(depth) {
+      switch (true) {
+        case depth > 5:
+          return "#810f7c";
+        case depth > 4:
+          return "#810f7c ";
+        case depth > 3:
+          return "#8856a7";
+        case depth > 2:
+          return "#8c96c6";
+        case depth > 1:
+          return "#b3cde3";
+        default:
+          return "#edf8fb";
+      }
+    }
+
+    L.geoJson(data, {
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+      style: styleInfo,
+      onEachLayer: function (feature, layer) {
+        layer.bindPopup(
+          "Magnitude: " +
+            feature.properties.mag +
+            "<br> Depth: " +
+            feature.geometry.coordinates[2]
+        );
+      },
+    }).addTo(map);
+
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "info legend"),
+        mags = [0, 1, 2, 3, 4, 5];
+
+      // loop through intervals and generate a colored square for each
+      for (var i = 0; i < mags.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' +
+          getColor(mags[i] + 1) +
+          '"></i> ' +
+          mags[i] +
+          (mags[i + 1] ? "&ndash;" + mags[i + 1] + "<br>" : "+");
+      }
+
+      return div;
+    };
+
+    legend.addTo(map);
+  });
+
